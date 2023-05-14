d75c6f72152c3c888ecd827667d02497730f63c0
guxtavow <82687503+guxtavow@users.noreply.github.com>
2023-05-13 21:43:22 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] my_array1 = {
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2165, 1457, 2456};
+
+        String[] my_array2 = {
+                "Java",
+                "Python",
+                "PHP",
+                "C#",
+                "C Programming",
+                "C++"
+        };
+
+
+
+//imprimindo as ARRAYS
+        //array1
+        System.out.println("Original: " + Arrays.toString(my_array1));
+        Arrays.sort(my_array1);
+        System.out.println("Ordenada: " + Arrays.toString(my_array1));
+        //array1
+
+        //espaçamento
+        System.out.println("");
+        System.out.println("");
+        //espaçamento
+
+        //array2
+        System.out.println("Original: " + Arrays.toString(my_array2));
+        Arrays.sort(my_array2);
+        System.out.println("Ordenada: " + Arrays.toString(my_array2));
+        //array2
+
+
+//imprimindo as ARRAYS
+
+
+
+
+    }
+}
